63c415689c5e91ca4179b32a543413d180888971
Bruce Williams <bruce@codefluency.com>
2009-05-18 20:30:23 -0500

    Initial commit to bitmask-attribute.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'bitmask_attribute'
+
+class Test::Unit::TestCase
+end
