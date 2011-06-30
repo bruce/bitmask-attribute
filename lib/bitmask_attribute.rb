db4866d86d1d787ddcabc70bbbf3c3429cb3da6b
Joel Moss <joel@developwithstyle.com>
2011-06-30 17:46:55 +0100

    Replaced #returning with #tap

--- lib/bitmask_attribute.rb
+++ lib/bitmask_attribute.rb
@@ -1,10 +1,10 @@
 require 'bitmask_attribute/value_proxy'
 
 module BitmaskAttribute
-  
   class Definition
     
     attr_reader :attribute, :values, :extension
+    
     def initialize(attribute, values=[], &extension)
       @attribute = attribute
       @values = values
@@ -20,110 +20,108 @@ module BitmaskAttribute
       create_named_scopes_on(model)
     end
     
-    #######
     private
-    #######
 
-    def validate_for(model)
-      # The model cannot be validated if it is preloaded and the attribute/column is not in the
-      # database (the migration has not been run).  This usually
-      # occurs in the 'test' and 'production' environments.
-      return if defined?(Rails) && Rails.configuration.cache_classes
+      def validate_for(model)
+        # The model cannot be validated if it is preloaded and the attribute/column is not in the
+        # database (the migration has not been run).  This usually
+        # occurs in the 'test' and 'production' environments.
+        return if defined?(Rails) && Rails.configuration.cache_classes
 
-      unless model.columns.detect { |col| col.name == attribute.to_s }
-        raise ArgumentError, "`#{attribute}' is not an attribute of `#{model}'"
-      end
-    end
-    
-    def generate_bitmasks_on(model)
-      model.bitmasks[attribute] = returning HashWithIndifferentAccess.new do |mapping|
-        values.each_with_index do |value, index|
-          mapping[value] = 0b1 << index
+        unless model.columns.detect { |col| col.name == attribute.to_s }
+          raise ArgumentError, "`#{attribute}' is not an attribute of `#{model}'"
         end
       end
-    end
     
-    def override(model)
-      override_getter_on(model)
-      override_setter_on(model)
-    end
-    
-    def override_getter_on(model)
-      model.class_eval %(
-        def #{attribute}
-          @#{attribute} ||= BitmaskAttribute::ValueProxy.new(self, :#{attribute}, &self.class.bitmask_definitions[:#{attribute}].extension)
-        end
-      )
-    end
-    
-    def override_setter_on(model)
-      model.class_eval %(
-        def #{attribute}=(raw_value)
-          values = raw_value.kind_of?(Array) ? raw_value : [raw_value]
-          self.#{attribute}.replace(values.reject(&:blank?))
-        end
-      )
-    end
-    
-    def create_convenience_class_method_on(model)
-      model.class_eval %(
-        def self.bitmask_for_#{attribute}(*values)
-          values.inject(0) do |bitmask, value|
-            unless (bit = bitmasks[:#{attribute}][value])
-              raise ArgumentError, "Unsupported value for #{attribute}: \#{value.inspect}"
-            end
-            bitmask | bit
+      def generate_bitmasks_on(model)
+        model.bitmasks[attribute] = HashWithIndifferentAccess.new.tap do |mapping|
+          values.each_with_index do |value, index|
+            mapping[value] = 0b1 << index
           end
         end
-      )
-    end
-
-
-    def create_convenience_instance_methods_on(model)
-      values.each do |value|
+      end
+    
+      def override(model)
+        override_getter_on(model)
+        override_setter_on(model)
+      end
+    
+      def override_getter_on(model)
         model.class_eval %(
-          def #{attribute}_for_#{value}?                  
-            self.#{attribute}?(:#{value})
+          def #{attribute}
+            @#{attribute} ||= BitmaskAttribute::ValueProxy.new(self, :#{attribute}, &self.class.bitmask_definitions[:#{attribute}].extension)
           end
         )
       end
-      model.class_eval %(
-        def #{attribute}?(*values)
-          if !values.blank?
-            values.all? do |value|
-              self.#{attribute}.include?(value)
-            end
-          else
-            self.#{attribute}.present?
-          end
-        end
-      )
-    end
     
-    def create_named_scopes_on(model)
-      model.class_eval %(
-        named_scope :with_#{attribute},
-          proc { |*values|
-            if values.blank?
-              {:conditions => '#{attribute} > 0 OR #{attribute} IS NOT NULL'}
-            else
-              sets = values.map do |value|
-                mask = #{model}.bitmask_for_#{attribute}(value)
-                "#{attribute} & \#{mask} <> 0"
+      def override_setter_on(model)
+        model.class_eval %(
+          def #{attribute}=(raw_value)
+            values = raw_value.kind_of?(Array) ? raw_value : [raw_value]
+            self.#{attribute}.replace(values.reject(&:blank?))
+          end
+        )
+      end
+    
+      def create_convenience_class_method_on(model)
+        model.class_eval %(
+          def self.bitmask_for_#{attribute}(*values)
+            values.inject(0) do |bitmask, value|
+              unless (bit = bitmasks[:#{attribute}][value])
+                raise ArgumentError, "Unsupported value for #{attribute}: \#{value.inspect}"
               end
-              {:conditions => sets.join(' AND ')}
+              bitmask | bit
             end
-          }
-        named_scope :without_#{attribute}, :conditions => "#{attribute} == 0 OR #{attribute} IS NULL"
-        named_scope :no_#{attribute},      :conditions => "#{attribute} == 0 OR #{attribute} IS NULL"
-      )
-      values.each do |value|
-        model.class_eval %(
-          named_scope :#{attribute}_for_#{value},
-                      :conditions => ['#{attribute} & ? <> 0', #{model}.bitmask_for_#{attribute}(:#{value})]
+          end
         )
-      end      
-    end
+      end
+
+
+      def create_convenience_instance_methods_on(model)
+        values.each do |value|
+          model.class_eval %(
+            def #{attribute}_for_#{value}?                  
+              self.#{attribute}?(:#{value})
+            end
+          )
+        end
+        model.class_eval %(
+          def #{attribute}?(*values)
+            if !values.blank?
+              values.all? do |value|
+                self.#{attribute}.include?(value)
+              end
+            else
+              self.#{attribute}.present?
+            end
+          end
+        )
+      end
+    
+      def create_named_scopes_on(model)
+        model.class_eval %(
+          named_scope :with_#{attribute},
+            proc { |*values|
+              if values.blank?
+                {:conditions => '#{attribute} > 0 OR #{attribute} IS NOT NULL'}
+              else
+                sets = values.map do |value|
+                  mask = #{model}.bitmask_for_#{attribute}(value)
+                  "#{attribute} & \#{mask} <> 0"
+                end
+                {:conditions => sets.join(' AND ')}
+              end
+            }
+          named_scope :without_#{attribute}, :conditions => "#{attribute} == 0 OR #{attribute} IS NULL"
+          named_scope :no_#{attribute},      :conditions => "#{attribute} == 0 OR #{attribute} IS NULL"
+        )
+        values.each do |value|
+          model.class_eval %(
+            named_scope :#{attribute}_for_#{value},
+                        :conditions => ['#{attribute} & ? <> 0', #{model}.bitmask_for_#{attribute}(:#{value})]
+          )
+        end      
+      end
     
   end
   
@@ -149,6 +147,5 @@ module BitmaskAttribute
       @bitmasks ||= {}
     end
       
-  end
-  
+  end  
 end
--- lib/bitmask_attribute/value_proxy.rb
+++ lib/bitmask_attribute/value_proxy.rb
@@ -1,5 +1,4 @@
 module BitmaskAttribute
-
   class ValueProxy < Array
       
     def initialize(record, attribute, &extension)
@@ -17,7 +16,7 @@ module BitmaskAttribute
     %w(push << delete replace reject! select!).each do |override|
       class_eval(<<-EOEVAL)
         def #{override}(*args)
-          returning(super) do
+          (super).tap do
             updated!
           end
         end
@@ -28,45 +27,42 @@ module BitmaskAttribute
       inject(0) { |memo, value| memo | @mapping[value] }
     end
   
-    #######
     private
-    #######
     
-    def validate!
-      each do |value|
-        if @mapping.key? value
-          true
-        else
-          raise ArgumentError, "Unsupported value for `#{@attribute}': #{value.inspect}"
+      def validate!
+        each do |value|
+          if @mapping.key? value
+            true
+          else
+            raise ArgumentError, "Unsupported value for `#{@attribute}': #{value.inspect}"
+          end
         end
       end
-    end
     
-    def updated!
-      validate!
-      uniq!
-      serialize!
-    end
+      def updated!
+        validate!
+        uniq!
+        serialize!
+      end
     
-    def serialize!
-      @record.send(:write_attribute, @attribute, to_i)
-    end
+      def serialize!
+        @record.send(:write_attribute, @attribute, to_i)
+      end
   
-    def extract_values
-      stored = [@record.send(:read_attribute, @attribute) || 0, 0].max
-      @mapping.inject([]) do |values, (value, bitmask)|
-        returning values do
-          values << value.to_sym if (stored & bitmask > 0)
-        end
-      end        
-    end
-    
-    def find_mapping
-      unless (@mapping = @record.class.bitmasks[@attribute])
-        raise ArgumentError, "Could not find mapping for bitmask attribute :#{@attribute}"
+      def extract_values
+        stored = [@record.send(:read_attribute, @attribute) || 0, 0].max
+        @mapping.inject([]) do |values, (value, bitmask)|
+          values.tap do
+            values << value.to_sym if (stored & bitmask > 0)
+          end
+        end        
       end
-    end
+    
+      def find_mapping
+        unless (@mapping = @record.class.bitmasks[@attribute])
+          raise ArgumentError, "Could not find mapping for bitmask attribute :#{@attribute}"
+        end
+      end
       
   end
-
 end
